91fc56d0311b3256a071c0dae1f31fc787ff96ec
Bhargav <bhargavvenkat515@gmail.com>
2024-06-15 15:35:04 +0530

    Added vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react()
+  ],
+  root: './', 
+  server: {
+    historyApiFallback: true, 
+  },
+  build: {
+    outDir: 'dist', 
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
+});
